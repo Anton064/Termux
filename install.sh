af327c9257b10f8115fa123d1d3e3ea774ba931a
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-08-31 14:20:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
 cd
-cp -r Termux
+cd ..
+cd usr/etc
+rm -rf bash.bashrc
+cd
+cd Termux 
+cp -r bash.bashrc /data/data/com.termux/files/usr/etc
+rm -rf Termux
+clear
+
+red ="\033[1;32m"
+
+printf "$red"
+echo "Start Termux app again" | pv -qL 7
+cd
+exit
