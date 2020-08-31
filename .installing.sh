08c344fa6af8bf3468b79aba2779b6fbf0666ed1
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-08-31 19:54:17 +0300

    Update and rename .install.sh to .installing.sh

--- .installing.sh
+++ .installing.sh
@@ -5,8 +5,8 @@ clear
 rm -rf bash.bashrc Termux
 clear
 git clone https://github.com/Anton064/Termux
-clear cd Termux && cp -r bash.bashrc .install /data/data/com.termux/files/usr/etc 
+clear cd Termux && cp -r bash.bashrc .installing.sh /data/data/com.termux/files/usr/etc 
 cp -r .message  /data/data/com.termux/files/home
 clear 
 cd
-./.message
+~/.message
