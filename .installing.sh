5fd183e0fac19f32e78626409e09958b8ac1eee1
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-09-01 10:03:24 +0300

    Update .installing.sh

--- .installing.sh
+++ .installing.sh
@@ -5,6 +5,6 @@ clear
 rm -rf bash.bashrc Termux
 clear
 git clone https://github.com/Anton064/Termux
-clear cd Termux && cp -r bash.bashrc .installing.sh /data/data/com.termux/files/usr/etc 
+clear cd Termux && cp -r .instaling bash.bashrc /data/data/com.termux/files/usr/etc 
 clear 
 cd
